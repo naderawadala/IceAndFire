b5d6a8be5c633d72a69a387f3e64cbc983e880a5
Nader <naderawadala@gmail.com>
2024-10-05 15:35:31 +0300

    add mongodbcontext

---
+++ IceAndFire.Infrastructure/Persistence/MongoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceAndFire.Infrastructure.Persistence
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+    }
+}
