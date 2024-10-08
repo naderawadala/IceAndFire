e702f8935acb9b1fee68783712012b6fb4115a35
Nader <naderawadala@gmail.com>
2024-10-08 09:58:47 +0300

    define entities

---
+++ IceAndFire.Domain/Entities/Book.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceAndFire.Domain.Entities
+{
+    public class Book
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Isbn { get; set; }
+        public List<string> Authors { get; set; } = new List<string>();
+        public int NumberOfPages { get; set; }
+        public string Publisher { get; set; }
+        public string Country { get; set; }
+        public string MediaType { get; set; }
+        public DateTime Released { get; set; }
+        public List<string> Characters { get; set; } = new List<string>();
+        public List<string> PovCharacters { get; set; } = new List<string>();
+    }
+}
---
+++ IceAndFire.Domain/Entities/House.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceAndFire.Domain.Entities
+{
+    public class House
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string CoatOfArms { get; set; }
+        public string Words { get; set; }
+        public List<string> Titles { get; set; } = new List<string>();
+        public List<string> Seats { get; set; } = new List<string>();
+        public string CurrentLord { get; set; }
+        public string Heir { get; set; }
+        public string Overlord { get; set; }
+        public string Founded { get; set; }
+        public string Founder { get; set; }
+        public string DiedOut { get; set; }
+        public List<string> AncestralWeapons { get; set; } = new List<string>();
+        public List<string> CadetBranches { get; set; } = new List<string>();
+        public List<string> SwornMembers { get; set; } = new List<string>();
+    }
+}
